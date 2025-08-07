4fde6a22fca6720a8dad993017bd8eeb0a037749
Clarissa Ortiaga <cortiaga@attune.gshapps.net>
2025-08-07 21:22:21 +0800

    Temporarily put the site under maintenance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,33 +6,33 @@ const Footer = () => {
         <div class="row">
           <div class="col-12 col-md">
             <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"/>
-            <small class="d-block mb-3 text-muted">&copy; 2024 VMC Hub.<br/> Created by <a target="_new" href="https://anthillc.github.io">AnthillC</a>. All rights reserved.</small>
+            <small class="d-block mb-3 text-muted">&copy; 2025 PhilTests.<br/> Created by <a target="_new" href="https://clrscr.github.io">Clarissa Ortiaga</a>. All rights reserved.</small>
           </div>
           <div class="col-6 col-md">
             <h5>Resources</h5>
             <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Quick Start</a></li>
-              <li><a class="text-muted" href="#">User Guide</a></li>
-              <li><a class="text-muted" href="#">FAQs</a></li>
-              <li><a class="text-muted" href="#">Report an Issue</a></li>
+              <li><a class="text-muted" href="/">Quick Start</a></li>
+              <li><a class="text-muted" href="/">User Guide</a></li>
+              <li><a class="text-muted" href="/">FAQs</a></li>
+              <li><a class="text-muted" href="/">Report an Issue</a></li>
             </ul>
           </div>
           <div class="col-6 col-md">
             <h5>Services</h5>
             <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">School Admissions Tesing</a></li>
-              <li><a class="text-muted" href="#">Human Resource Evaluatio</a></li>
-              <li><a class="text-muted" href="#">Psychological Assessment</a></li>
-              <li><a class="text-muted" href="#">Workshops</a></li>
+              <li><a class="text-muted" href="/">School Admissions Tesing</a></li>
+              <li><a class="text-muted" href="/">Human Resource Evaluatio</a></li>
+              <li><a class="text-muted" href="/">Psychological Assessment</a></li>
+              <li><a class="text-muted" href="/">Workshops</a></li>
             </ul>
           </div>
           <div class="col-6 col-md">
             <h5>About</h5>
             <ul class="list-unstyled text-small">
-              <li><a class="text-muted" href="#">Team</a></li>
-              <li><a class="text-muted" href="#">Locations</a></li>
-              <li><a class="text-muted" href="#">Terms</a></li>
-              <li><a class="text-muted" href="#">Contact</a></li>
+              <li><a class="text-muted" href="/">Team</a></li>
+              <li><a class="text-muted" href="/">Locations</a></li>
+              <li><a class="text-muted" href="/">Terms</a></li>
+              <li><a class="text-muted" href="/">Contact</a></li>
             </ul>
           </div>
         </div>
--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -4,8 +4,8 @@ const UnderConstruction = () => {
     return (
         <div class="main">
             <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                <h1 class="display-4">Coming Soon</h1>
-                <p class="lead">This page is still under construction.</p>
+                <h1>🚧 Under Construction 🚧</h1>
+                <p class="lead">We are working hard to bring you something amazing.</p>
             </div>
       </div>
     )
